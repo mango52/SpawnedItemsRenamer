6ed3e902521619eb91361befdac20112c2a90b76
mangodev <mango@phoenixgaming.in>
2013-02-18 15:19:49 -0500

    items spawned now have lore with spawner's name
    decided to use lore instead of naming the item

--- src/com/eodomo/SpawnedItemsRenamer/SpawnedItemsRenamer.java
+++ src/com/eodomo/SpawnedItemsRenamer/SpawnedItemsRenamer.java
@@ -1,6 +1,6 @@
 package com.eodomo.SpawnedItemsRenamer;
 
-import java.util.logging.Logger;
+import java.util.ArrayList;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
@@ -11,64 +11,85 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class SpawnedItemsRenamer extends JavaPlugin{
-	public final Logger logger = this.getLogger();
+public final class SpawnedItemsRenamer extends JavaPlugin {
 
 	@Override
 	public void onDisable() {
-		this.logger.info(this.getDescription().getName() + " has been disabled!");
+		this.getLogger().info(this.getDescription().getName() + " has been disabled!");
 	}
 
 	@Override
 	public void onEnable() {
-		this.logger.info(this.getDescription().getName() + " version " + this.getDescription().getVersion() + " has been enabled!");
+		this.getLogger().info(this.getDescription().getName() + " version " + this.getDescription().getVersion() + " has been enabled!");
 	}
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String args[]) {
-		if (command.getName().equals("i")) {
-			if (sender instanceof Player){
+		if (command.getName().equalsIgnoreCase("i")) {
+			if (sender instanceof Player) {
 				Player player = (Player) sender;
-				if (player.hasPermission("spawneditemsrenamer.rename")) {
-					if (player.getItemInHand().getTypeId() != 0) {
-						if (args.length > 0) {
-							Material material = Material.matchMaterial(args[1]);
-							player.getInventory().addItem(new ItemStack(material));
-							ItemStack is = player.getItemInHand();
-							ItemMeta im = is.getItemMeta();
-							im.setDisplayName(player.getDisplayName() + " - " + args[1]);
-							is.setItemMeta(im);
+				if (player.hasPermission("spawneditemsrenamer.item")) {
+					Material material;
+					int amount;
+					
+					if (args.length == 1) {
+						material = Material.matchMaterial(args[0]);
+						amount = 1;
+					} else if (args.length == 2) {
+						material = Material.matchMaterial(args[0]);
+						try {
+							amount = Integer.parseInt(args[1]);
+						} catch (NumberFormatException e) {
+							player.sendMessage(ChatColor.DARK_RED + args[1] + " is not a valid number!");
+							return false;
 						}
+					} else
+						return false;
+					
+					if (material == null) {
+						player.sendMessage(ChatColor.DARK_RED + args[0] + " is not a valid item!");
+						return false;
 					}
+					
+					ItemStack stack = new ItemStack(material, amount);
+					ItemMeta meta = stack.getItemMeta();
+					ArrayList<String> lore = new ArrayList<String>(); //test start
+					lore.add(player.getName());
+					meta.setLore(lore);
+					stack.setItemMeta(meta);
+					player.getInventory().addItem(stack);
 				} else {
 					player.sendMessage(ChatColor.DARK_RED + "You don't have permission to use this command.");
+					return false;
 				}
 			} else {
 				sender.sendMessage(ChatColor.DARK_RED + "Only players can use this command.");
+				return false;
 			}
 		}
 
-		if (command.getName().equals("give")) {
-			if (sender instanceof Player) {
+		if (command.getName().equalsIgnoreCase("give")) {
+			if (sender instanceof Player){
 				Player player = (Player) sender;
-				if (player.hasPermission("spawneditemsrenamer.rename")) {
-					if (player.getItemInHand().getTypeId() != 0) {
-						if (args.length > 0) {
-							Material material = Material.matchMaterial(args[1]);
-							player.getInventory().addItem(new ItemStack(material));
-							ItemStack is = player.getItemInHand();
-							ItemMeta im = is.getItemMeta();
-							im.setDisplayName(player.getDisplayName() + " - " + args[1]);
-							is.setItemMeta(im);
-						}
-					}
+				if (player.hasPermission("spawneditemsrenamer.give")) {
+					if ((args.length > 0) && (args.length < 3)) {
+						Material material = Material.matchMaterial(args[0]);
+						ItemStack stack = new ItemStack(material);
+						stack.getItemMeta().setDisplayName("test - " + player.getName());
+						player.getInventory().addItem(stack);
+						return true;
+					} else
+						return false;
 				} else {
 					player.sendMessage(ChatColor.DARK_RED + "You don't have permission to use this command.");
+					return false;
 				}
 			} else {
 				sender.sendMessage(ChatColor.DARK_RED + "Only players can use this command.");
+				return false;
 			}
 		}
+
 		return false;
 	}
 }
